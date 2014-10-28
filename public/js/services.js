1251fd557cd61bf197b82e71c027bc8b99d49595
Juan Bonfante <jbonfante@enova.com>
2014-10-28 18:05:37 -0500

    1st commit for meetup files, still have a ton of cleanup to do.
    Module [overlord]:
      - ngKeypad.js
      * app.js

--- public/js/services.js
+++ public/js/services.js
@@ -2,22 +2,6 @@
  * Created by jbonfante on 8/26/14.
  */
 
-//var bombServices = ['Activation', 'Deactivation', 'DetonationTime'];
-//
-//for (var __service in bombServices)
-//{
-//    bombApp.factory(__service, function($resource) {
-//        return $resource("/"+__service.toLowerCase()+"/:code", { code: "@"+__service.toLowerCase()+"code" },
-//            {
-//                'create':  { method: 'POST' },
-//                'index':   { method: 'GET', isArray: true },
-//                'show':    { method: 'GET', isArray: false },
-//                'update':  { method: 'PUT' },
-//                'destroy': { method: 'DELETE' }
-//            }
-//        );
-//    });
-//}
 
 bombApp.factory("Bomb", function($resource) {
     return $resource("/api/bomb",{},
